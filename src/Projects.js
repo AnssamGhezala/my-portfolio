bc09e69876a79b23c687ddfbcb371112185e4dc3
Anssam Ghezala <ghezala.anssam@gmail.com>
2019-03-05 04:06:09 -0500

    add projects

--- src/Projects.js
+++ src/Projects.js
@@ -3,16 +3,31 @@ import "./App.css";
 import Item from "./Item";
 
 class Projects extends Component {
+  handleClick = thing => {
+    if (thing === "Stopwatch") {
+      console.log(thing);
+      window.open("https://github.com/AnssamGhezala/stopwatch");
+    } else if (thing === "Todo") {
+      console.log(thing);
+      window.open("https://github.com/AnssamGhezala/todo-list");
+    } else if (thing === "Search") {
+      console.log(thing);
+      window.open("https://github.com/AnssamGhezala/searchBar");
+    } else if (thing === "Resto") {
+      console.log(thing);
+      window.open("https://github.com/W2018-ECSE223/Group01");
+    }
+  ***REMOVED***
   render() {
     return (
       <div className="Projects">
         {" "}
-        <div className="project">
+        <div className="project" onClick={() => this.handleClick("Stopwatch")}>
           {" "}
           <div className="title"> Stopwatch </div>{" "}
           <div className="ProjectDescription"> Simple React stopwatch :)</div>{" "}
         </div>
-        <div className="project">
+        <div className="project" onClick={() => this.handleClick("Resto")}>
           {" "}
           <div className="title"> RestoApp </div>{" "}
           <div className="ProjectDescription">
@@ -20,7 +35,7 @@ class Projects extends Component {
             Java Restoration app using MVC model
           </div>{" "}
         </div>
-        <div className="project">
+        <div className="project" onClick={() => this.handleClick("Search")}>
           {" "}
           <div className="title"> Search Bar </div>{" "}
           <div className="ProjectDescription">
@@ -28,7 +43,7 @@ class Projects extends Component {
             Simple React search bar supporting txt files :)
           </div>{" "}
         </div>
-        <div className="project">
+        <div className="project" onClick={() => this.handleClick("Todo")}>
           <div className="title"> Todo list</div>{" "}
           <div className="ProjectDescription">
             {" "}
