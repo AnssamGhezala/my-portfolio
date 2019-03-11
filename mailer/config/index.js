547a611b1a704947712f70f2db52c6a84a8fc99a
Anssam Ghezala <ghezala.anssam@gmail.com>
2019-03-11 11:54:03 -0400

    change refresh token

--- mailer/config/index.js
+++ mailer/config/index.js
@@ -1,10 +1,10 @@
 const user = "ghezala.anssam@gmail.com";
-const refreshToken = "1/ljSF4pye1O-Awtar9s8in1GOdU3fXHliCVmqpaF4fs0";
+const refreshToken = "1/uekwQhki5SPRzF18PTLUXbDw01uRaSctQ9860bnhAv0";
 const accessToken =
-  "ya29.GlvJBmpJoqpdYwSKyXIpGGQIp94tkYI1B5gCSBuzRUVDnYhbRPHSwCOWjXnTaCdiDXVcxloVM4A0vWZSdDCyiEBQZ5lSHe8XPp5hNi56C0U4KcyEQl-g9i3AtV4I";
+  "ya29.GlvJBvzCJ41p4cU82aoiXoCDYA77Uf2btOoyg4hgXru0SaTcY4_Wo62Risi8iL9ydeWxjim0WC9ou18gur6DVRTsm-rgz56VzQy8-2QDqDjt8PFNKpNWdm8wqgHp";
 const clientId =
   "574704717585-8mhh52fsqs5vfp22ae0tscivuh42uupi.apps.googleusercontent.com";
-const clientSecret = "TeIT7YlSI3kt3CyKWw2wfBwi";
+const clientSecret = "puakRmzDHNHDzjzAGbf50GaD";
 
 ***REMOVED***
 ***REMOVED***
