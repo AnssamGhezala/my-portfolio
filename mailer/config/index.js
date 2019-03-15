9aafbfba2e9a7d84accf55c578f7f1829d09736c
Anssam Ghezala <ghezala.anssam@gmail.com>
2019-03-15 11:16:37 -0400

    add sample in configs

--- mailer/config/index.js
+++ mailer/config/index.js
@@ -1,10 +1,8 @@
 const user = "ghezala.anssam@gmail.com";
-const refreshToken = "1/uekwQhki5SPRzF18PTLUXbDw01uRaSctQ9860bnhAv0";
-const accessToken =
-  "ya29.GlvJBvzCJ41p4cU82aoiXoCDYA77Uf2btOoyg4hgXru0SaTcY4_Wo62Risi8iL9ydeWxjim0WC9ou18gur6DVRTsm-rgz56VzQy8-2QDqDjt8PFNKpNWdm8wqgHp";
-const clientId =
-  "574704717585-8mhh52fsqs5vfp22ae0tscivuh42uupi.apps.googleusercontent.com";
-const clientSecret = "puakRmzDHNHDzjzAGbf50GaD";
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
